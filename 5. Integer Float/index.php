435d364205593b2b72c63375f0ee78ddbbbb3bd6
Audyari Wiyono <audy123ari@gmail.com>
2024-11-04 14:42:44 +0700

    5. Integer Float

---
+++ 5. Integer Float/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Integer Float</title>
+</head>
+<body>
+
+<?php
+
+$a = 10;
+$b = 3;
+$c = $a / $b;
+
+echo $a;
+echo "<br>";
+echo $b;
+echo "<br>";
+echo $c;
+echo "<br>";
+echo round($c);
+echo "<br>";
+echo round($c,2);
+
+echo "<br>";
+echo ++$a;
+echo "<br>";
+echo --$a;
+
+echo "<br>";
+echo --$a;
+
+?>
+
+</body>
+</html>
