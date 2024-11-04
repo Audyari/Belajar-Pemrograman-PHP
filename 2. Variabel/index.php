264f5552b868c8ae1e1869f51218356ed3c0eaec
Audyari Wiyono <audy123ari@gmail.com>
2024-11-04 09:11:55 +0700

    2. Variabel

---
+++ 2. Variabel/index.php
@@ -0,0 +1,17 @@
+<?php
+
+$nilai1 = 1000;
+$nilai2 = 2000;
+
+echo $nilai1;
+echo "<br>";
+echo $nilai2;
+echo "<br>";
+
+echo $nilai1 + $nilai2;
+
+$namaLengkap = "Audyari Wiyono";
+echo "<br>";
+echo $namaLengkap;
+
+?>
