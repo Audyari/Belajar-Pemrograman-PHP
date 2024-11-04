c1329b083b0495e692f862da867e8d19f8f6bf76
Audyari Wiyono <audy123ari@gmail.com>
2024-11-04 13:11:04 +0700

    4. Mengenal Fungsi

---
+++ 4. Mengenal Fungsi/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mengenal Fungsi</title>
+</head>
+<body>
+    <?php 
+    // Define the name variable
+    $nama = "<h1> audyari wiyono</h1>";
+
+    // Output the name in different cases
+    echo ucwords($nama) . "<br>";
+    echo strtoupper($nama) . "<br>";
+    echo strtolower($nama);
+    ?>
+</body>
+</html>
