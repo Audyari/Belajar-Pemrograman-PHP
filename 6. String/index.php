7ea26e1a8d7aa5ada36d30a8fc621dcc1e91b344
Audyari Wiyono <audy123ari@gmail.com>
2024-11-04 16:23:07 +0700

    6. String

---
+++ 6. String/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>String</title>
+</head>
+<body>
+    
+<?php
+$nama = "Audyari Wiyono";
+$pesan = "Saya adalah seorang mahasiswa nama saya $nama";
+echo $pesan . "<br>" . $nama;
+echo "<br>";
+
+
+
+$nama = 'Budi Waluyo';
+$pesan = 'Saya adalah seorang mahasiswa nama saya ' . $nama;
+echo $pesan . '<br>' . $nama;
+
+$namaPertama = "Chandra";
+$namaKedua = "Dikitaku";
+
+echo "<br>";
+
+echo $namaPertama . " Adalah " . $namaKedua;
+?>
+
+</body>
+</html>
