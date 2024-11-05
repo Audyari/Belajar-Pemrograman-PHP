293fd8df686ecdbae36e7866116a59486efc24ab
Audyari Wiyono <audy123ari@gmail.com>
2024-11-05 14:28:10 +0700

    7. Boolean

---
+++ 7. Boolean/index.php
@@ -0,0 +1,31 @@
+<?php
+
+$tipe_Boolean = true;
+
+echo $tipe_Boolean;
+echo "<br>";
+var_dump($tipe_Boolean);
+
+$a = 10;
+$b = 20;
+$c = $a > $b;
+echo "<br>";
+var_dump($c);
+
+echo "<br>";
+
+var_dump($a + $b);
+
+echo "<br>";
+
+var_dump($a == $b);
+
+echo "<br>";
+
+var_dump($a != $b);
+
+echo "<br>";
+
+var_dump($a === $b);
+
+?>
