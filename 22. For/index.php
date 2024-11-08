5764d5c380e3b0b2bd52f1f40c1feb2d7165cf98
Audyari Wiyono <audy123ari@gmail.com>
2024-11-08 13:20:03 +0700

    22. For

---
+++ 22. For/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>For</title>
+</head>
+<body>
+
+<?php
+// Perulangan for untuk menampilkan nomor antrian dari 0 hingga 1000
+for ($x = 0; $x < 10; $x++) {
+    // Menampilkan nomor antrian dengan format "Nomor Antrian yang tersedia: $x"
+    echo "Nomor Antrian yang tersedia: $x <br>";
+}
+?>
+
+</body>
+</html>
