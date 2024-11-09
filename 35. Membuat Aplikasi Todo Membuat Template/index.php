13818e7ff573f49e9b3c2f5d1ad006f6d0c0cdb2
Audyari Wiyono <audy123ari@gmail.com>
2024-11-10 00:56:51 +0700

    39. Membuat Aplikasi Todo Menghapus Data

--- 35. Membuat Aplikasi Todo Membuat Template/index.php
+++ 35. Membuat Aplikasi Todo Membuat Template/index.php
@@ -24,8 +24,6 @@ if (file_exists('todo.txt')) {
     }
 }
 
-
-
 // Memeriksa apakah ada data todo yang dikirim melalui metode POST
 if (isset($_POST['todo'])) {
     // Mengambil data yang diinput pada form
@@ -37,36 +35,57 @@ if (isset($_POST['todo'])) {
         'status' => 0
     ];
 
-    // Menyimpan daftar belanja dalam format serialized
+    // Menyimpan daftar belanja dalam format JSON
     $daftar_belanja = json_encode($todos);
 
-    // Menulis data serialized ke dalam file todo.txt
-    file_put_contents('todo.txt', $daftar_belanja);
-
-    // Redirect ke halaman index.php
-    // Setelah selesai menambahkan data Todo baru, maka
-    // browser akan diarahkan kembali ke halaman index.php
-    // untuk menampilkan daftar Todo yang sudah diupdate
-    header('Location: index.php');
-    exit;
+    // Menulis data JSON ke dalam file todo.txt
+    simpanData($daftar_belanja);
 }
 
-//jika ditemukan $_GET['status']
+// Memeriksa apakah ada parameter status yang dikirim melalui metode GET
 if (isset($_GET['status'])) {
-    //ubah status
+    // Mengubah status todo berdasarkan key yang diberikan
     $todos[$_GET['key']]['status'] = $_GET['status'];
+
+    // Menyimpan perubahan ke dalam file todo.txt
     $daftar_belanja = json_encode($todos);
+    simpanData($daftar_belanja);
+}
+
+/**
+ * Simpan daftar belanja dalam format JSON ke dalam file todo.txt
+ *
+ * @param string $daftar_belanja Daftar belanja dalam format JSON
+ *
+ * @return void
+ */
+function simpanData($daftar_belanja)
+{
+    // Simpan daftar belanja dalam format JSON ke dalam file todo.txt
     file_put_contents('todo.txt', $daftar_belanja);
-    //redirect halaman
+
+    // Redirect ke halaman index.php setelah selesai menyimpan data
     header('location:index.php');
 }
+
+// Menampilkan array todos untuk tujuan debugging
 print_r($todos);
+
+// Memeriksa apakah ada parameter hapus yang dikirim melalui metode GET
+if (isset($_GET['hapus'])) {
+    // Hapus data todo berdasarkan key yang diberikan
+    unset($todos[$_GET['key']]);
+
+    // Menyimpan perubahan data ke dalam file todo.txt
+    $daftar_belanja = json_encode($todos);
+    simpanData($daftar_belanja);
+}
+
 ?>
 
-
 <!-- 
-  Dokumen HTML untuk aplikasi Todo sederhana
-  Menggunakan Bootstrap untuk styling
+  HTML Document for a simple Todo application
+  Using Bootstrap for styling
 -->
 
 <!DOCTYPE html>
@@ -74,72 +93,67 @@ print_r($todos);
 
 <head>
     <!-- 
-    Pengaturan karakter encoding dan viewport untuk responsif
-  -->
+    Character encoding and viewport settings for responsiveness
+    -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- 
-    Judul halaman
-  -->
+    Page title
+    -->
     <title>Aplikasi Todos</title>
 
     <!-- 
-    Menggunakan Bootstrap CSS untuk styling
-  -->
+    Include Bootstrap CSS for styling
+    -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 </head>
 
 <body>
     <!-- 
-    Container untuk aplikasi Todo
-  -->
+    Container for the Todo application
+    -->
     <div class="container mt-5">
         <!-- 
-      Judul aplikasi Todo
-    -->
+        Title of the Todo application
+        -->
         <h1 class="text-center">Todo App</h1>
 
         <!-- 
-      Formulir untuk memasukkan item Todo baru
-    -->
+        Form to input a new Todo item
+        -->
         <form action="" method="post">
             <div class="form-group">
                 <!-- 
-          Label dan input untuk memasukkan item Todo
-        -->
+                Label and input for entering a Todo item
+                -->
                 <label for="todo">Daftar Belanja Hari ini</label>
                 <input type="text" class="form-control" id="todo" name="todo" placeholder="Masukkan todo">
             </div>
             <!-- 
-        Tombol untuk menyimpan item Todo
-      -->
+            Button to save the Todo item
+            -->
             <button type="submit" class="btn btn-primary">Simpan</button>
         </form>
 
-
-
-
-
+        <!-- 
+        List group to display Todo items
+        -->
         <ul class="list-group mt-3">
             <!-- 
-            Looping data array $todos yang berisi item Todo
-            dan menampilkan setiap item Todo
-          -->
+            Loop through the $todos array to display each Todo item
+            -->
             <?php foreach ($todos as $key => $value): ?>
                 <li class="list-group-item">
-
                     <!-- 
-                    Membuat input checkbox untuk mengubah status Todo
-                    Ketika input checkbox di klik, maka akan mengarahkan ke halaman index.php
-                    dengan parameter status yang berbeda dan key yang sesuai dengan item Todo yang di klik
+                    Checkbox input to toggle Todo status
+                    When clicked, redirects to index.php with toggled status and the corresponding Todo item key
                     -->
-
-                    <input type="checkbox" name="todo" onclick="window.location.href='index.php?status=<?php echo $value['status'] == 1 ? '0' : '1'; ?> &key=<?php echo $key; ?>'" <?php if ($value['status'] == 1) echo 'checked' ?>>
-
+                    <input type="checkbox" name="todo" onclick="window.location.href='index.php?status=<?php echo $value['status'] == 1 ? '0' : '1'; ?>&key=<?php echo $key; ?>'" <?php if ($value['status'] == 1) echo 'checked' ?>>
 
                     <label>
                         <?php
+                        // Display Todo with strikethrough if completed
                         if ($value['status'] == 1) {
                             echo '<del>' . $value['todo'] . '</del>';
                         } else {
@@ -147,18 +161,17 @@ print_r($todos);
                         }
                         ?></label>
                     <!-- 
-                    Membuat link untuk menghapus item Todo
-                  -->
-                    <a href="#" class="float-right">hapus</a>
+                    Link to delete the Todo item
+                    -->
+                    <a href="index.php?hapus=1&key=<?php echo $key; ?>" onclick="return confirm('Apakah Anda Yakin akan menghapus data ini?')">hapus</a>
                 </li>
             <?php endforeach; ?>
         </ul>
-
     </div>
 
     <!-- 
-    Menggunakan jQuery dan Bootstrap JavaScript untuk fungsionalitas
-  -->
+    Include jQuery and Bootstrap JavaScript for functionality
+    -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </body>
